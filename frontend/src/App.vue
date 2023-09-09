6e1a282d215a0e248ce757aa6e2385dad3f5df09
su83 <su83@drexel.edu>
2023-09-09 10:55:19 -0400

    added button before camera loads up

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,27 @@
+<template>
+  <div>
+    <h1 class="title has-text-centered mt-5">VisionPro</h1>
+    
+    <!-- Button to start camera -->
+    <button @click="startCamera">Start Camera</button>
+
+    <!-- Camera element, displayed conditionally -->
+    <camera v-if="showCamera" :resolution=autoplay></camera>
+  </div>
+</template>
+
 <script>
 export default {
   name: "App",
-}
+  data() {
+    return {
+      showCamera: false, // Initially hide the camera
+    };
+  },
+  methods: {
+    startCamera() {
+      this.showCamera = true; // Show the camera when the button is clicked
+    },
+  },
+};
 </script>
-  
-<template>
-  <h1 class="title has-text-centered mt-5">VisionPro</h1>
-  <camera autoplay></camera>
-</template>
-
