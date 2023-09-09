1e1c43caf9feb603107e35de2e04790bb5aaabb8
su83 <su83@drexel.edu>
2023-09-09 00:29:31 -0400

    fixed bug where camera was zooming in more than necessary

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,6 +6,6 @@ export default {
   
 <template>
   <h1 class="title has-text-centered mt-5">VisionPro</h1>
-  <camera :resolution="{ width: 375, height: 812 }" autoplay></camera>
+  <camera autoplay></camera>
 </template>
 
